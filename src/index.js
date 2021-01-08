a89feadd4d9e0d7ccafb91cf05d51766a52f7d9c
Your Name <your_email@whatever.com>
2021-01-08 17:14:31 +0300

    Redux homework

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const reducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INC':
+            return state + 1
+        case 'DEC':
+            return state - 1
+        case 'REV':
+            state = 0
+            return state
+        default:
+            return state
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = createStore(reducer)
+
+const update = () => {
+    document.querySelector('.count').textContent = store.getState()
+}
+
+store.subscribe(update)
+
+const inc = () => ({type: 'INC'})
+const dec = () => ({type: 'DEC'})
+const rev = () => ({type: 'REV'})
+
+
+document.querySelector('.inc').addEventListener('click', () => {
+    store.dispatch(inc())
+})
+
+
+document.querySelector('.dec').addEventListener('click', () => {
+    store.dispatch(dec())
+})
+
+document.querySelector('.rev').addEventListener('click', () => {
+    store.dispatch(rev())
+})
+
+
+// const reducer = (state = 0, action) => {
+//     switch (action.type) {
+//         case 'INC':
+//             return state + 1
+//         case 'DEC':
+//             return state - 1
+//         case 'RND':
+//             return state + action.value
+
+//         default:
+//             return state
+//     }
+// }
+
+// const inc = () =>  ({type: 'INC'})
+// const dec = () =>  ({type: 'DEC'})
+// const rnd = (value) =>  ({type: 'RND', value})
+
+
+// const store = createStore(reducer)
+
+// document.getElementById('inc').addEventListener('click', () => {
+//     store.dispatch(inc())
+// })
+
+// document.getElementById('dec').addEventListener('click', () => {
+//     store.dispatch(dec())
+// })
+
+// document.getElementById('rnd').addEventListener('click', () => {
+
+// const value = Math.floor(Math.random() * 10)
+//     store.dispatch(rnd(value))
+// })
+// const update = () => {
+//     document.getElementById('counter').textContent = store.getState()
+// }
+
+
+// store.subscribe(update)
+
